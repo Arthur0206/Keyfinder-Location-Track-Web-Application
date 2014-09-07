d0da491da27e95278324f2f3a7878e35a246be01
Arthur Chen <s810011@gmail.com>
2014-09-07 01:08:08 -0700

    Change userMain.php to show location described in "data HTML5 attribute": http://jquery-ui-map.googlecode.com/svn/trunk/demos/jquery-google-maps-data-attribute.html 1. in server side, userMain.php get a date file and inserts all longtitude/latitude/datetime entries into a <ul>, where each entry is a <li>, up to 96 entries. 2. in client side, the javascript in userMain.php will parse those entries and show them on map.

--- userMain.php
+++ userMain.php
@@ -12,12 +12,12 @@ include "algor.php";
 		<meta http-equiv="content-language" content="en" />
 		<meta name="author" content="Johan Säll Larsson" />
 		<meta name="viewport" content="width=device-width,initial-scale=1" />
-		<meta name="keywords" content="Google maps, jQuery, plugin, extend, pagination" />
-		<meta name="description" content="An example how to use pagination and extensions with Google maps jQuery plugin" />
+		<meta name="keywords" content="Google maps, jQuery, plugin, HTML5, Data" />
+		<meta name="description" content="An example how to use jQuery data HTML5 attribute with Google maps jQuery plugin" />
 		<link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
-		<meta name="DC.title" content="xample with Google maps and jQuery - Google maps jQuery plugin" />
-		<meta name="DC.subject" content="Google maps;jQuery;plugin;extend;pagination" />
-		<meta name="DC.description" content="An example how to use pagination and extensions with Google maps jQuery plugin" />
+		<meta name="DC.title" content="Example with jQuery data HTML5 attribute - Google maps jQuery plugin" />
+		<meta name="DC.subject" content="Google maps;jQuery;plugin;HTML5;Data" />
+		<meta name="DC.description" content="An example how to use jQuery data HTML5 attribute with Google maps jQuery plugin" />
 		<meta name="DC.creator" content="Johan Säll Larsson" />
 		<meta name="DC.language" content="en" />
 		<link type="text/css" rel="stylesheet" href="css/960/min/960.css" />
@@ -32,7 +32,34 @@ include "algor.php";
 			.pagination .back-btn { background:url("images/arrow_left_12x12.png") no-repeat 50% 50%; border-right: 1px solid #ccc; }
 			.pagination .fwd-btn { background:url("images/arrow_right_12x12.png") no-repeat 50% 50%; border-left: 1px solid #ccc; }
 		</style>
+
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
+		<script type="text/javascript" src="js/jquery-1.7.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/underscore-1.2.2/underscore.min.js"></script>
+		<script type="text/javascript" src="js/backbone-0.5.3/backbone.min.js"></script>
+		<script type="text/javascript" src="js/prettify/prettify.min.js"></script>
+		<script type="text/javascript" src="js/demo.js"></script>
+		<script type="text/javascript" src="ui/jquery.ui.map.js"></script>
 		<script type="text/javascript" src="js/modernizr-2.0.6/modernizr.min.js"></script>
+		<script type="text/javascript">
+            $(function() { 
+				demo.add(function() {
+					$('#map_canvas').gmap({'disableDefaultUI':true, 'callback': function() {
+						var self = this;
+						$("[data-gmapping]").each(function(i,el) {
+							var data = $(el).data('gmapping');
+							self.addMarker({'id': data.id, 'tags':data.tags, 'position': new google.maps.LatLng(data.latlng.lat, data.latlng.lng), 'bounds':true }, function(map,marker) {
+								$(el).click(function() {
+									$(marker).triggerEvent('click');
+								});
+							}).click(function() {
+								self.openInfoWindow({ 'content': /*$(el).find('.info-box').text()*/ data.time }, this);
+							});
+						});						
+					}});
+				}).load();
+			});
+        </script>
     </head>
     <body style="-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none;">
 		<header class="dark">
@@ -54,94 +81,25 @@ include "algor.php";
 				<div class="item rounded dark">
 					<div id="map_canvas" class="map rounded"></div>
 				</div>
+
+				<ul style="visibility:hidden">
+					<li data-gmapping='{"id":"m_1","latlng":{"lat":27.6648274,"lng":-81.51575350000002},"tags":"drupal","time":"Feb 11 2012"}'>
+						<p class="info-box">Florida DrupalCamp - Feb 11 2012</p>
+					</li>
+					<li data-gmapping='{"id":"m_2","latlng":{"lat":1.352083,"lng":103.81983600000001},"tags":"drupal","time":"Feb 11 2012"}'>
+						<p class="info-box">DrupalCamp Singapore - Mar 03 2012</p>					
+					</li>
+					<li data-gmapping='{"id":"m_3","latlng":{"lat":39.7391536,"lng":-104.9847034},"tags":"drupal","time":"Feb 11 2012"}'>
+						<p class="info-box">DrupalCon 2012 Denver - Mar 20 2012</p>				
+					</li>
+					<li data-gmapping='{"id":"m_4","latlng":{"lat":36.1658899,"lng":-86.7844432},"tags":"drupal","time":"Feb 11 2012"}'>
+						<p class="info-box">DrupalCamp Nashville - Apr 28 2012</p>				
+					</li>
+					<li data-gmapping='{"id":"m_5","latlng":{"lat":55.6760968,"lng":12.568337100000008},"tags":"drupal","time":"Feb 11 2012"}'>
+						<p class="info-box">DrupalCamp Copenhagen 5.0 - May 11 2012</p>				
+					</li>
+				</ul>
 			</article>
 		</div>
-		
-		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
-		<script type="text/javascript" src="js/jquery-1.7.1/jquery.min.js"></script>
-		<script type="text/javascript" src="js/underscore-1.2.2/underscore.min.js"></script>
-		<script type="text/javascript" src="js/backbone-0.5.3/backbone.min.js"></script>
-		<script type="text/javascript" src="js/prettify/prettify.min.js"></script>
-		<script type="text/javascript" src="js/demo.js"></script>
-		<script type="text/javascript" src="ui/jquery.ui.map.js"></script>
-		<script type="text/javascript" src="ui/jquery.ui.map.services.js"></script>
-		<script type="text/javascript" src="ui/jquery.ui.map.extensions.js"></script>
-		<script type="text/javascript">
-            $(function() { 
-				demo.add(function() {
-					var markers = [
-						{'position': '59.32893000000001,18.064910000000054', 'title': 'Stockholm, Sweden' },
-						{'position': '35.6894875,139.69170639999993', 'title': 'Tokyo, Japan' },
-						{'position': '13.7234186, 100.47623190000002', 'title': 'Bangkok, Thailand' },
-						{'position': '51.508129,-0.12800500000003012', 'title': 'London, Great Britain' },
-						{'position': '40.7143528,-74.0059731', 'title': 'New York, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '34.0522342,-118.2436849', 'title': 'Los Angeles, USA' },
-						{'position': '55.75,37.616666699999996', 'title': 'Moskva, Ryssia' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '35.6894875,139.69170639999993', 'title': 'Tokyo, Japan' },
-						{'position': '13.7234186, 100.47623190000002', 'title': 'Bangkok, Thailand' },
-						{'position': '51.508129,-0.12800500000003012', 'title': 'London, Great Britain' },
-						{'position': '40.7143528,-74.0059731', 'title': 'New York, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '34.0522342,-118.2436849', 'title': 'Los Angeles, USA' },
-						{'position': '55.75,37.616666699999996', 'title': 'Moskva, Ryssia' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Test, France' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '59.32893000000001,18.064910000000054', 'title': 'Stockholm, Sweden' },
-						{'position': '35.6894875,139.69170639999993', 'title': 'Tokyo, Japan' },
-						{'position': '13.7234186, 100.47623190000002', 'title': 'Bangkok, Thailand' },
-						{'position': '51.508129,-0.12800500000003012', 'title': 'London, Great Britain' },
-						{'position': '40.7143528,-74.0059731', 'title': 'New York, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '34.0522342,-118.2436849', 'title': 'Los Angeles, USA' },
-						{'position': '55.75,37.616666699999996', 'title': 'Moskva, Ryssia' },
-						{'position': '59.32893000000001,18.064910000000054', 'title': 'Stockholm, Sweden' },
-						{'position': '35.6894875,139.69170639999993', 'title': 'Tokyo, Japan' },
-						{'position': '13.7234186, 100.47623190000002', 'title': 'Bangkok, Thailand' },
-						{'position': '51.508129,-0.12800500000003012', 'title': 'London, Great Britain' },
-						{'position': '40.7143528,-74.0059731', 'title': 'New York, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '34.0522342,-118.2436849', 'title': 'Los Angeles, USA' },
-						{'position': '55.75,37.616666699999996', 'title': 'Moskva, Ryssia' },
-						{'position': '59.32893000000001,18.064910000000054', 'title': 'Stockholm, Sweden' },
-						{'position': '35.6894875,139.69170639999993', 'title': 'Tokyo, Japan' },
-						{'position': '13.7234186, 100.47623190000002', 'title': 'Bangkok, Thailand' },
-						{'position': '51.508129,-0.12800500000003012', 'title': 'London, Great Britain' },
-						{'position': '40.7143528,-74.0059731', 'title': 'New York, USA' },
-						{'position': '48.856614,2.3522219000000177', 'title': 'Paris, France' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-						{'position': '40.7143528,-74.0059731', 'title': 'San Diego, USA' },
-					];
-					$('#map_canvas').gmap({'zoom': 5, 'disableDefaultUI':true, 'callback': function() {
-						var self = this;
-						$.each(markers, function(i, marker) {
-							self.addMarker(marker).click(function() {
-								self.openInfoWindow({'content': this.title}, this);
-							});
-						});
-					}}).gmap('pagination', 'title');
-				}).load();
-			});
-        </script>
-    
 	</body>
 </html>
