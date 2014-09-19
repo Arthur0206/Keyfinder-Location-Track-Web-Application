5ab8f446badbcdaba0b614add687492e94869da4
PCH <p7huang@gmail.com>
2014-09-18 19:43:56 -0700

    remove connection (replace by db_connect and db_config)

--- connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connection = mysql_connect("localhost", "root", "sosoman1984") or die("Couldn't connect to the server!");
-mysql_select_db("location_service", $connection) or die("Couldn't connect to the database!");
-?>
