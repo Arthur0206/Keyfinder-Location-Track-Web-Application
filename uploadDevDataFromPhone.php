82729d74d750a9ebd785bfa23e315fe7d98d9975
PCH <p7huang@gmail.com>
2014-09-27 16:49:03 -0700

    bug fix, call devDataParser before logSysErrMsg

--- uploadDevDataFromPhone.php
+++ uploadDevDataFromPhone.php
@@ -3,11 +3,11 @@ include "devDataParser.php";
 
 $request_body = file_get_contents('php://input');
 $jsonObj = json_decode($request_body, true);
-
+$response = devDataParser($jsonObj);
 if ($response["success"] == 0) {
     logSysErrMsg($response["message"]);
 }
 
 // reply back to the phone
-echo json_encode(devDataParser($jsonObj));
+echo json_encode($response);
 ?>
